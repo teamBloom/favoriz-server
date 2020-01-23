669b8d0c7c26f7f9eb96becf7254c156b326a82d
KimHyoJin <hyojin03131995@gmail.com>
2020-01-24 01:00:54 +0900

    NO-ISSUE Add default name for wish list

--- src/main/kotlin/com/team/bloom/favoriz/controller/model/V1WishListWithProductIds.kt
+++ src/main/kotlin/com/team/bloom/favoriz/controller/model/V1WishListWithProductIds.kt
@@ -1,8 +1,10 @@
 package com.team.bloom.favoriz.controller.model
 
+import java.time.OffsetDateTime
+
 data class V1WishListWithProductIds(
     val id: Long,
-    val name: String,
+    val name: String? = OffsetDateTime.now().toString(),
     val userId: Long,
     val productIds: List<Long>
 )
